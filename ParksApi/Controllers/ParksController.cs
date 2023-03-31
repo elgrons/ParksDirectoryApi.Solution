87c7047fb5341392184f513cc2a8166c58e06b1c
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-31 09:59:12 -0700

    update ParksController.cs with CRUD

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,148 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksApi.Models;
+
+namespace ParksApiController.Controllers
+
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParksApiContext _db;
+
+    public ParksController(ParksApiContext db)
+    {
+      _db = db;
+    }
+
+    // Pagination Code:
+    [HttpGet("page/{page}")]
+    public async Task<ActionResult<List<Park>>> GetPages(int page)
+    {
+        if (_db.Parks == null)
+        return NotFound();
+
+      int pageCount = _db.Parks.Count();
+
+      var parks = await _db.Parks
+        .Skip((page - 1) * pageSize)
+        .Take(pageSize)
+        .ToListAsync();
+
+      var response = new ParkResponse
+      {
+        Destinations = destinations,
+        //page number inside the url
+        CurrentPage = page,
+        //the amount of parks returned from the database
+        Pages = pageCount,
+        //amnt of items on the page
+        PageSize = pageSize
+      };
+      return Ok(response);
+    }
+	
+    [HttpGet]
+    public async Task<List<Park>> Get( string name, string location, string review, int rating)
+    {
+      
+      IQueryable<Park> query = _db.Parks.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (location != null)
+      {
+        query = query.Where(entry => entry.Location == location);
+      }
+
+      if (review != null)
+      {
+        query = query.Where(entry => entry.Review == review);
+      }
+
+      if (rating >= 0)
+      {
+        query = query.Where(entry => entry.Rating >= rating);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    //Get: api/Parks/1
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      Park parl = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(park);
+    }
+
+    //POST api/parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+
+    //PUT: api/Parks/2
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Parks.Update(park);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(location => location.ParkId == id);
+    }
+
+    //DELETE: api/Parks/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
---
+++ ParksApi/Migrations/20230331164932_Initial.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParksApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Location = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Review = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Location", "Name", "Rating", "Review" },
+                values: new object[,]
+                {
+                    { 1, "Oregon", "Crater Lake National Park", 10, "Great" },
+                    { 2, "Washington", "Olympic National Park", 8, "Excellent" },
+                    { 3, "Washington", "San Juan Island National Historic Park", 7, "Very Good" },
+                    { 4, "Florida", "Everglades National Park", 8, "Awesome, but hot" },
+                    { 5, "Utah", "Dead Horse Point State Park", 8, "Nice" },
+                    { 6, "Alaska", "Chugach State Park", 6, "Pretty" },
+                    { 7, "Florida", "Canaveral National Seashore", 6, "Pretty" },
+                    { 8, "Nevada", "Valley of Fire State Park", 8, "Gorgeous, but warm during the day and cold at night" },
+                    { 9, "Minnesota", "Voyageurs National Park", 6, "Remote and low average visitor count" },
+                    { 10, "Utah", "Bryce Canyon National Park", 1, "Too orange, too spiky" },
+                    { 11, "Hawaii", "Haleakalā National Park", 10, "Perfect" },
+                    { 12, "Wyoming", "Yellowstone National Park", 9, "Great geyser!" },
+                    { 13, "Arizona", "Saguaro National Park", 8, "Lots of cacti!" },
+                    { 14, "Tennessee", "Great Smoky Mountains National Park", 2, "Nothing specific to do" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
